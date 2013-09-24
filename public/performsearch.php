bcf8b31b575b53c28ae157c90b766a82b73b930c
John Faver <johncfaver@gmail.com>
2013-09-24 11:48:08 -0400

    fix search bug

--- public/performsearch.php
+++ public/performsearch.php
@@ -1,9 +1,12 @@
 <?php
 
-//Perform search in database. Place resulting molids into session.
-//Send user to displaysearch.php with results in $_SESSION['search_result'] array
+//Perform search in database. Place resulting molids into session variable.
+//Send user to displaysearch.php with results in $_SESSION['search_result'] array.
 
-    $debug=0;
+//The search_result session variable is an array of molids, unless a similarity
+//       search was performed. In this case, it is an associative array of
+//       molid=>similarity
+
     require('../private/cred.php');
     try{
         $dbconn = new PDO("pgsql:dbname=$dbname;host=$dbhost;port=$dbport",$dbuser,$dbpass);
@@ -11,21 +14,26 @@
         echo 'Database connection failed: '. $e->getMessage();
     }
     $dbconn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING );
-    
+  
+    try{
+        $nproc=exec("nproc");
+    }catch(Exception $e){
+        $nproc = 1;
+    }
+
     session_start();
-    $loggedin = isset($_SESSION['username']);
-    if(!$loggedin) returnhome();
+    if(!isset($_SESSION['username'])) returnhome();
 
     if(isset($_SESSION['search_results'])) unset($_SESSION['search_results']);
 
-    //Max number of results desired.
+    //Max number of results desired. This limits our search. 0 implies no limit.
     $nummol=(isset($_POST['nummol']) and $_POST['nummol']>0)?(int)$_POST['nummol']:0; 
 
 //Query parameters-constants
-    //Max difference between query weight and molweight to count as a match
+    //Max difference between query weight and molweight to count as a match.
     $arbitrary_weight_cutoff = 50;
     //Max Tanimoto coefficient for structure similarity search.
- 	$similaritythreshold=(isset($_GET['similaritythreshold']))?(real)$_GET['similaritythreshold']:0.4;
+ 	$similaritythreshold=(isset($_GET['similaritythreshold']))?(real)$_GET['similaritythreshold']:0.3;
 //Query parameters-input
     $query_molname  =(isset($_POST['query_molname']))?pg_escape_string($_POST['query_molname']):false;
     $query_molweight=(isset($_POST['query_molweight']))?pg_escape_string($_POST['query_molweight']):false;
@@ -33,12 +41,12 @@
 //Parse molweight query, looking for range queries e.g. 500-560.
     if($query_molweight){
         $hyphenpos = strpos($query_molweight,'-');
-        $query_molweight_range = $hyphenpos!==false;  //IS this a range query?
+        $query_molweight_range = ($hyphenpos!==false);  //IS this a range query?
         if($query_molweight_range){
             $query_molweight_lower= (int)substr($query_molweight,0,$hyphenpos);
             $query_molweight_upper= (int)substr($query_molweight,$hyphenpos+1);
             if($query_molweight_upper==0){
-                $query_molweight_upper=10000;
+                $query_molweight_upper=100000;
             }
         }else{
             $query_molweight= (int)$query_molweight;
@@ -52,7 +60,7 @@
     //Is there a non-empty structure for structure search?
     $query_structure=($moltext && count($moltext)>6)?true:false; 
     $searchtype=(isset($_POST['searchtype']))?$_POST['searchtype']:0;
-
+    //Array of molids to be included in structure search.
     $structure_search_molids = Array();
 
 //**************SEARCH PART*******************************************************
@@ -60,22 +68,22 @@
 //Let's grab the molids that match the metadata
     if($query_molname or $query_molweight or $query_target){   
         if($query_target){
-            $qstr = 'SELECT distinct d.molid from moldata d left join molecules m on m.molid=d.molid where d.targetid=';
+            $qstr = 'SELECT DISTINCT d.molid FROM moldata d LEFT JOIN molecules m ON m.molid=d.molid WHERE d.targetid=';
             $qstr.= $query_target;
         }else{
-            $qstr = 'SELECT distinct m.molid from molecules m where molname is not null';    
+            $qstr = 'SELECT DISTINCT m.molid FROM molecules m WHERE molname IS NOT NULL';    
         }
         if($query_molname){
-            $qstr.=' and (m.molname ~* :str1 or m.iupac ~* :str1 or m.cas ~* :str1) ';
+            $qstr.=' AND (m.molname ~* :str1 OR m.iupac ~* :str1 OR m.cas ~* :str1) ';
         }
         if($query_molweight_range){
-            $qstr.=' and m.molweight between :num1 and :num2 ';
+            $qstr.=' AND m.molweight BETWEEN :num1 and :num2 ';
         }        
         if(!$query_molweight_range && $query_molweight){
-            $qstr.=' and abs(m.molweight-:num3) < '.$arbitrary_weight_cutoff;
+            $qstr.=' AND abs(m.molweight-:num3) < '.$arbitrary_weight_cutoff;
         }
         if($nummol){
-            $qstr.=' limit :num4 ';
+            $qstr.=' LIMIT :num4 ';
         }
             
         $q = $dbconn->prepare($qstr); 
@@ -99,19 +107,17 @@
             array_push($structure_search_molids,$r['molid']);
         }
     }else{
-        $q = $dbconn->query("SELECT molid FROM molecules");
+        $q = $dbconn->query("SELECT DISTINCT molid FROM molecules");
         while($r = $q->fetch(PDO::FETCH_ASSOC)){;
             array_push($structure_search_molids,$r['molid']);
         }
     }
 
- 
     if($query_structure){
       //Number of structures to search
         $nmolsearch = count($structure_search_molids);
       //Number of worker processes
         $nworkers = $nproc-1; 
-        $njobs = $nworkers;
       //Number of comparisons per worker; divide comparisons equally amongst workers, but cap at maxperjob
         $nmolperworker = ceil($nmolsearch/$nworkers);
         $maxperjob = 500;
@@ -134,22 +140,19 @@
             $similarities = Array();
             while(count($oarray)<$njobs){
                 $ijob = count($oarray);
-                for($i=$ijob*$nworkers;$i<($ijob+1)*$nworkers;$i++){
-                    if($i>$njobs){
-                         break;
-                    }
+                for($i=$ijob;$i<($ijob+$nworkers);$i++){
+                    if($i>=$njobs) break;
                     $cmdstr = 'for i in';
                     for($j=$i*$nmolperworker;$j<($i+1)*$nmolperworker;$j++){
-                        if($j>=$nmolsearch){
-                             break;
-                        }
+                        if($j>=$nmolsearch) break;
                         $cmdstr.=' '.$structure_search_molids[$j];
                     }
                     $cmdstr.=';do '.$babeldir.'babel uploads/structures/$i.mol '.$tempmolfile.' -ofpt 2>/dev/null |';
                     $cmdstr.=" grep Tanimoto | awk -v m=\$i '{if(NF>5 && $6>".$similaritythreshold.")print m\" \"$6}';done";
                     array_push($parray,popen($cmdstr,'r'));
                 }
-                for($i=$ijob*$nworkers;$i<($ijob+1)*$nworkers;$i++){
+                for($i=$ijob;$i<($ijob+$nworkers);$i++){
+                    if($i>=$njobs) break;
                     array_push($oarray,stream_get_contents($parray[$i]));
                 }
             }
@@ -160,10 +163,10 @@
                     $t = explode(" ",$r);
                     if(count($t)>1){
                         $similarities[$t[0]]=round($t[1],2);
-                        if($nummol and count($similarities>$nummol)) break;
+                        if($nummol and count($similarities>=$nummol)) break;
                     }
                 }
-                if($nummol and count($similarities>$nummol)) break;
+                if($nummol and count($similarities>=$nummol)) break;
             }
             //$similarities now is an associative array with molid=>similarity.
             $_SESSION['search_results']=$similarities;
@@ -175,19 +178,19 @@
             $oarray = Array();
             while(count($oarray)<$njobs){
                 $ijob = count($oarray);
-                for($i=$ijob*$nworkers;$i<($ijob+1)*$nworkers;$i++){
+                for($i=$ijob;$i<($ijob+$nworkers);$i++){
+                    if($i>=$njobs) break;
                     $cmdstr = 'for i in';
                     for($j=$i*$nmolperworker;$j<($i+1)*$nmolperworker;$j++){
-                        if($j>=$nmolsearch){
-                            break;
-                        }
+                        if($j>=$nmolsearch) break;
                         $cmdstr.=' '.$structure_search_molids[$j];
                     }
                     $cmdstr.=';do echo -n $i" "; '.$babeldir.'babel uploads/structures/$i.mol -s'.$tempmolfile.' /tmp/'.session_id().$i.'results.smi -xt 2>/dev/null;';
                     $cmdstr.='[ -s /tmp/'.session_id().$i.'results.smi ] && echo 1 || echo 0;done';
                     array_push($parray,popen($cmdstr,'r'));
                 }
-                for($i=$ijob*$nworkers;$i<($ijob+1)*$nworkers;$i++){
+                for($i=$ijob;$i<($ijob+$nworkers);$i++){
+                    if($i>=$njobs) break;
                     array_push($oarray,stream_get_contents($parray[$i]));
                 }
             }
@@ -197,10 +200,10 @@
                     $t = explode(" ",$r);
                     if(count($t)>1 and $t[1]=='1'){
                         array_push($superstructures,$t[0]);
-                        if($nummol and count($superstructures)>$nummol) break;
+                        if($nummol and count($superstructures)>=$nummol) break;
                     }
                 }
-                if($nummol and count($superstructures)>$nummol) break;
+                if($nummol and count($superstructures)>=$nummol) break;
             }
             //$superstructures now has every element as a molid which is a substructure of the query.
             $_SESSION['search_results'] = $superstructures;
