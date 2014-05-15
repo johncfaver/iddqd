762db066124504346685ef5617065586f72bc9d3
John C. Faver <johncfaver@gmail.com>
2014-05-15 15:47:37 -0400

    Fix bug in molecule name suggestions

--- cgi-bin/ajax-getlastmolname.py
+++ cgi-bin/ajax-getlastmolname.py
@@ -45,8 +45,7 @@ try:
     #   (Then search for all strings of digits, \d+, in the remainder. Take the first match.)
     if (seriesprefix):
         re1 = '^'+seriesprefix
-        re2 = '^'+seriesprefix+'.*\d+'
-        q.execute("SELECT (regexp_matches(regexp_replace(molname,%s,''),'\d+'))[1] from molecules where molname ~ %s order by molname  DESC limit 1",[re1,re2])
+        q.execute("SELECT (regexp_matches(molname,'\d+$'))[1]::int as t from molecules where molname ~ %s order by t DESC limit 1",[re1])
         if q.rowcount == 1:
             r=q.fetchone()
             lastentry=r[0]
