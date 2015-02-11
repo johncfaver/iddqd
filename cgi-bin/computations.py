ea34b32998669a4823fe4a17a841fc8bf5ed0690
John Faver <john.faver@yale.edu>
2015-02-11 11:18:45 -0500

    ensure that 3d mol files are neutral

--- cgi-bin/computations.py
+++ cgi-bin/computations.py
@@ -16,9 +16,18 @@ molid = int(sys.argv[1])
 
 ###GENERATE 3D MOL FILE WITH OBGEN###
 os.chdir('../public/uploads/structures')
+#3D conformer search
 subprocess.call([config.babeldir+'obgen',str(molid)+'.mol'],stdout=open(str(molid)+'-3dt.mol','w'),stderr=open(os.devnull,'w'))
+#Remove warning flags
 subprocess.call(['/bin/grep','-v','WARNING',str(molid)+'-3dt.mol'],stdout=open(str(molid)+'-3d.mol','w'),stderr=open(os.devnull,'w'))
+#Convert to PDB without hydrogens
+subprocess.call([config.babeldir+'babel','-imol',str(molid)+'-3d.mol','-d','-opdb',str(molid)+'-3d.pdb'],stdout=open(os.devnull,'w'),stderr=open(os.devnull,'w'))
+#Remove charged atoms (necessary for qikprop)
+subprocess.call(['/bin/sed','-i',r"s/1[\+-]$//g",str(molid)+'-3d.pdb'],stdout=open(os.devnull,'w'),stderr=open(os.devnull,'w'))
+#Finally convert to 3D mol with hydrogens in neutral state
+subprocess.call([config.babeldir+'babel','-ipdb',str(molid)+'-3d.pdb','-h','-omol',str(molid)+'-3d.mol'],stdout=open(os.devnull,'w'),stderr=open(os.devnull,'w'))
 os.remove(str(molid)+'-3dt.mol')
+os.remove(str(molid)+'-3d.pdb')
 molobj = molecule(str(molid)+'-3d.mol')
 os.chdir(cgidir)
 
