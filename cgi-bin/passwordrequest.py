99d5ff76123992df6dddb9e4322f2fcf90b0ccbe
John C. Faver <johncfaver@gmail.com>
2015-03-04 11:27:43 -0500

    further formatting

--- cgi-bin/passwordrequest.py
+++ cgi-bin/passwordrequest.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-##
+# 
 # passwordrequest.py
-#   Runs when user requests a password change.
+# Runs when user requests a password change.
 #
-##
 
-import psycopg2,cgi,cgitb,string,random
-from sys import exit
-cgitb.enable(display=0,logdir="../log/",format="text")
+import sys
+import cgi,cgitb
+import string
+import random
+import psycopg2
 import config
+
+cgitb.enable(display=0,logdir="../log/",format="text")
 
 form=cgi.FieldStorage()
 keys=form.keys()
@@ -20,45 +23,54 @@ else:
     email=0
 if not email:
     config.returnhome(33)
-    exit()
+    sys.exit()
 
 try:
     dbconn=psycopg2.connect(config.dsn)
     q=dbconn.cursor()
 
-#Get userid to match this email address
+    #Get userid matching this email address
     q.execute('SELECT userid FROM users WHERE email=%s',[email])
     if(q.rowcount==0):
         print 'Location: ../changepasswordrequestpage.php?status=bademail \n\n'
-        exit()    
+        sys.exit()    
     r = q.fetchone() 
     userid = str(r[0])
-#Check for open requests from this user in the last 24 hours.
-    q.execute('SELECT daterequested from passwordchanges p where p.userid=%s and p.datechanged is null and extract(day from localtimestamp-p.daterequested) < 1', [userid])
+    #Check for open requests from this user in the last 24 hours.
+    q.execute("""SELECT daterequested 
+                    FROM passwordchanges p 
+                    WHERE p.userid=%s 
+                        AND p.datechanged IS NULL 
+                        AND extract(day from localtimestamp-p.daterequested) < 1
+              """, [userid])
     if(q.rowcount!=0):
-        print 'Location: ../changepasswordrequestpage.php?status=openrequest'
-        print ''
-        exit()
+        print 'Location: ../changepasswordrequestpage.php?status=openrequest \n\n'
+        sys.exit()
 
-#This now appears to be a valid request.
-#End past requests. Unused requests get datechanged set to localtime and the changed variable remains false.
+    #This now appears to be a valid request.
+    #End past requests. Unused requests get datechanged set to localtime and the changed variable remains false.
     q.execute('UPDATE passwordchanges SET datechanged=localtimestamp WHERE userid=%s',[userid])
-#Create new request
+    
+    #Create new request
     changekey = ''.join(random.sample(string.ascii_letters + string.digits,50))
     q.execute('INSERT INTO passwordchanges (userid,daterequested,changekey) VALUES(%s,localtimestamp,%s)',[userid,changekey])
     dbconn.commit()
     q.close()
     dbconn.close()
-#Send email to user
-    mailstr="Hi, \n"
-    mailstr+="\tBelow is a URL for your password reset at "+config.domain+". It will expire in 24 hours. \n\n"
-    mailstr+=config.domain
-    mailstr+="/changepasswordpage.php?key="+changekey+" \n\n\n\n"
-    mailstr+="(This was an automated message from "+config.domain+")"
+   
+    #Send email to user
+    url = config.domain+"/changepasswordpage.php?key="+changekey
+    mailstr="""Hi,
+
+    Below is a URL for your password reset at {}. It will expire in 24 hours. 
+
+    {}
+
+
+    (This was an automated message from {})
+            """.format(config.domain,url,config.domain)
     config.sendemail(email,mailstr)  
-    print 'Location: ../index.php'
-    print ''
+    config.returnhome(0)
 except Exception: 
     config.returnhome(34)
-    print ''
 
